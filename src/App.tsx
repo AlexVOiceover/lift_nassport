a9ab403f0c600d37041bc1f220b95aa44b120998
Alexander <alex@alexvoiceover.com>
2025-01-21 21:24:02 +0000

    fixed deployment options

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,21 @@ import {
 } from 'react-router-dom';
 import SentenceBuilderPage from './pages/employees/SentenceBuilder';
 import EmployerDashboardPage from './pages/employers/EmployersDashboard';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/LIFT-frontend'>
       <div className='App'>
         <Routes>
+          {/* Entry point */}
+          <Route path='/' element={<HomePage />} />
           {/* Define routes for employees and employers */}
           <Route path='/employees' element={<SentenceBuilderPage />} />
           <Route path='/employers' element={<EmployerDashboardPage />} />
 
           {/* Redirect to the employee page by default */}
-          <Route path='*' element={<Navigate to='/employees' replace />} />
+          <Route path='*' element={<Navigate to='/' replace />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className='min-h-screen bg-gray-800 text-white flex flex-col items-center justify-center'>
+      <h1 className='text-3xl font-bold mb-8'>Welcome to the LIFT App</h1>
+      <div className='flex space-x-4'>
+        <Link
+          to='/employees'
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        >
+          Employee Dashboard
+        </Link>
+        <Link
+          to='/employers'
+          className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'
+        >
+          Employer Dashboard
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;

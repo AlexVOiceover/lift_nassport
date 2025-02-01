26379db9d127b60b7d2d1b4d315982ef3fe38bb5
Alexander <alex@alexvoiceover.com>
2025-02-01 18:56:14 +0000

    Restored base on vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/LIFT-frontend/',
   // base: '/LIFT-frontend/',
   plugins: [react()],
 });
